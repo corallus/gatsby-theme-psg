c2c907082c829c26a3f6ff4b0e761607be7b3c59
Vincent van Bergen <v.vanbergen@gmail.com>
2019-09-05 19:50:23 +0200

    ticket link in modal

--- src/components/event/tickets/Button.js
+++ src/components/event/tickets/Button.js
@@ -1,11 +1,31 @@
-import React from 'react'
-import { Button } from 'react-bootstrap';
+import React, { useState } from 'react'
+import { Button, Modal } from 'react-bootstrap';
 import { MdArrowForward } from 'react-icons/md';
 
-export default ({url}) => {
-    return (
-        <Button href={url} className="btn btn-info">
-          Koop ticket <MdArrowForward size={32} />
-        </Button>
-    )
+export default ({ url }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button onClick={handleShow} className="btn btn-info">
+        Koop ticket <MdArrowForward size={32} />
+      </Button>
+      <Modal size="xl" show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Tickets kopen</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <iframe width="100%" height="800px" frameBorder="0" src={url}></iframe>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Sluiten
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
 }
