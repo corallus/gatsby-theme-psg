059ae1005d0260d1ecc83071eb9215e33de16ccc
Vincent <vincent@shoppingminds.com>
2020-12-16 17:55:49 +0100

    bugfix

--- src/components/Tickets/Eventix/index.js
+++ src/components/Tickets/Eventix/index.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState, useEffect} from 'react'
 import {Helmet} from 'react-helmet'
 import Context from "../../Events/Context";
 import {ticketParams} from "../../../params";
@@ -7,16 +7,26 @@ const Tickets = () => {
     const {state} = useContext(Context)
     const {event} = state
 
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        document.querySelectorAll('iframe').forEach(
+            function(elem){
+                elem.parentNode.removeChild(elem);
+            });
+    }, [event])
+
     if (!event.frontmatter.eventix) return <h3 className="text-center">{ticketParams.emptyText}</h3>
 
     return (
         <>
             <Helmet>
-                <script src="https://shop.eventix.io/build/integrate.js"/>
+                <script src={`https://shop.eventix.io/build/integrate.js?event=${event.frontmatter.eventix}`}/>
             </Helmet>
             <div id="shop-frame"
-    data-url={`https://shop.eventix.io/${event.frontmatter.eventix}`}
-    style={{maxWidth: '600px', margin: '0 auto'}}/>
+                 data-url={`https://shop.eventix.io/${event.frontmatter.eventix}`}
+                 style={{maxWidth: '600px', margin: '0 auto'}}
+            />
         </>
     )
 }
