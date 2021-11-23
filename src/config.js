7bedbd99f6a409cf03156ac2df709ea9bdf3b171
Vincent van Bergen <v.vanbergen@gmail.com>
2021-11-23 22:57:34 +0100

    gatsby env vars

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ import {createConfig} from 'gatsby-theme-psg';
 const config = createConfig({
     backend: {
         name: 'github',
-        branch: process.env.BRANCH,
-        repo: process.env.REPO,
+        branch: process.env.GATSBY_BRANCH,
+        repo: process.env.GATSBY_REPO,
         base_url: 'https://infaxka6yj.execute-api.eu-central-1.amazonaws.com',
         auth_endpoint: '/prod/auth'
     },
