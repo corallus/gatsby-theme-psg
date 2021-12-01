5e82e85057979758ac378051e1e5c2e70762a0c9
Vincent van Bergen <v.vanbergen@gmail.com>
2021-12-01 20:52:11 +0100

    info hook bug fix

--- src/components/Info/hook.js
+++ src/components/Info/hook.js
@@ -4,7 +4,12 @@ import Context from "../Events/Context";
 const useInfo = (items) => {
     const {state} = useContext(Context)
     const {event} = state
-    return items.filter(post => !post.frontmatter.events || (post.frontmatter.events.filter(ev => ev?.id === event.id).length))
+
+    return items.filter(post =>
+        !post.frontmatter.events ||
+        post.frontmatter.events.length === 0 ||
+        post.frontmatter.events.some(ev => ev.id === event.id)
+    )
 }
 
 export default useInfo
