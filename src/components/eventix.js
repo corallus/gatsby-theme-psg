baba562f6fdaed6752aed767215c09d8a4a20d65
Vincent van Bergen <v.vanbergen@gmail.com>
2022-02-11 02:59:30 +0100

    remove offline mode

--- src/components/eventix.js
+++ src/components/eventix.js
@@ -1,18 +1,11 @@
-import React, {useContext, useEffect} from 'react'
-import {Helmet} from 'react-helmet'
+import React, {useContext} from 'react'
 import Context from 'gatsby-theme-psg/src/components/Events/Context'
 import {ticketParams} from "gatsby-theme-psg/src/params";
+import {Helmet} from "react-helmet";
 
 const Tickets = () => {
     const {state} = useContext(Context)
     const {event} = state
-
-    useEffect(() => {
-        document.querySelectorAll('iframe').forEach(
-            function(elem){
-                elem.parentNode.removeChild(elem);
-            });
-    }, [event])
 
     if (!event.frontmatter.eventix) return <h3 className="text-center">{ticketParams.emptyText}</h3>
 
@@ -21,11 +14,17 @@ const Tickets = () => {
             <Helmet>
                 <script src={`https://shop.eventix.io/build/integrate.js?event=${event.frontmatter.eventix}`}/>
             </Helmet>
-            <div id="shop-frame"
-                 data-url={`https://shop.eventix.io/${event.frontmatter.eventix}`}
-                 style={{maxWidth: '600px', margin: '0 auto'}}
-            />
+            <Display event={event} />
         </>
+    )
+}
+
+const Display = ({event}) => {
+    return (
+        <div id="shop-frame"
+             data-url={`https://shop.eventix.io/${event.frontmatter.eventix}`}
+             style={{maxWidth: '600px', margin: '0 auto'}}
+        />
     )
 }
 
