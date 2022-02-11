2aa5684f2df9a357e9377b319a7d4ea6098c212b
Vincent van Bergen <v.vanbergen@gmail.com>
2022-02-11 03:01:20 +0100

    something

--- src/components/eventix.js
+++ src/components/eventix.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react'
+import React, {useContext} from 'react'
 import Context from 'gatsby-theme-psg/src/components/Events/Context'
 import {ticketParams} from "gatsby-theme-psg/src/params";
 import {Helmet} from "react-helmet";
